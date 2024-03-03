97dcc2494d435d34602cfd9912e31cc64064b1f8
Cédric Anthony <Anthodev@users.noreply.github.com>
2024-03-03 18:58:37 +0100

    ✅ test(ulidgen): added some tests (#4)

--- src/main.rs
+++ src/main.rs
@@ -81,3 +81,83 @@ fn handle_cmd(cmd: Command) -> String {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use ulid::Ulid;
+    use super::*;
+
+    #[test]
+    fn test_parse_cmd() {
+        let args = vec![
+            OsString::from("ulidgen"),
+            OsString::from("-t"),
+            OsString::from("ulid"),
+        ];
+
+        let command = parse_cmd(args);
+
+        assert_eq!(command.generator, "ulid");
+        assert_eq!(command.option, "");
+        assert_eq!(command.value, "");
+    }
+
+    #[test]
+    fn test_parse_cmd_with_option() {
+        let args = vec![
+            OsString::from("ulidgen"),
+            OsString::from("-t"),
+            OsString::from("ulid"),
+            OsString::from("-c"),
+            OsString::from("time"),
+            OsString::from("01"),
+        ];
+
+        let command = parse_cmd(args);
+
+        assert_eq!(command.generator, "ulid");
+        assert_eq!(command.option, "time");
+        assert_eq!(command.value, "01");
+    }
+
+    #[test]
+    fn test_handle_cmd() {
+        let command = Command {
+            generator: "ulid".to_string(),
+            option: "".to_string(),
+            value: "".to_string(),
+        };
+
+        let output = handle_cmd(command);
+
+        assert!(Ulid::from_string(output.as_str()).is_ok());
+    }
+
+    #[test]
+    fn test_handle_cmd_with_option() {
+        let command = Command {
+            generator: "ulid".to_string(),
+            option: "uuid".to_string(),
+            value: "".to_string(),
+        };
+
+        let output = handle_cmd(command);
+
+        let uuid_format_regex = regex::Regex::new(r"^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89ABab][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$").unwrap();
+        assert!(uuid_format_regex.is_match(output.as_str()));
+    }
+
+    #[test]
+    fn test_handle_cmd_with_option_time() {
+        let command = Command {
+            generator: "ulid".to_string(),
+            option: "time".to_string(),
+            value: Ulid::new().to_string(),
+        };
+
+        let output = handle_cmd(command);
+
+        let atom_format_regex = regex::Regex::new(r"^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$").unwrap();
+        assert!(atom_format_regex.is_match(output.as_str()));
+    }
+}
