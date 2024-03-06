0ada850455a23775e20172438293e4f72b785c83
Cédric Anthony <Anthodev@users.noreply.github.com>
2024-03-06 23:04:28 +0100

    ✨ feat(system): add content to clipboard after result (#7)

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod sfgen;
 mod cmd_handler;
 
 use std::env;
+use arboard::Clipboard;
 
 fn main() {
     let command = cmd_handler::parse_cmd(env::args_os().collect());
@@ -18,5 +19,31 @@ fn main() {
     }
 
     let output = cmd_handler::handle_cmd(command);
+
     println!("{}", output);
+
+    add_to_clipboard(output);
+}
+
+fn add_to_clipboard(output: String) {
+    let mut clipboard_ctx = Clipboard::new().unwrap();
+    clipboard_ctx.set_text(output.to_owned()).unwrap();
+
+    println!("Copied to clipboard");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add_to_clipboard() {
+        let output = "test".to_string();
+        add_to_clipboard(output);
+
+        let mut clipboard_ctx = Clipboard::new().unwrap();
+        let clipboard_content = clipboard_ctx.get_text().unwrap();
+
+        assert_eq!(clipboard_content, "test");
+    }
 }
